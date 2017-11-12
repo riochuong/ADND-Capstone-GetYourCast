0feabfe05c1d3a8728a71137472d6b8b36ed50f3
chuongdao <cd.kernel.dev.105@gmail.com>
2017-11-11 21:18:15 -0800

    move control of each recycler view to viewholder

--- app/src/main/java/getyourcasts/jd/com/getyourcasts/view/adapter/SearchPodcastRecyclerViewAdapter.kt
+++ app/src/main/java/getyourcasts/jd/com/getyourcasts/view/adapter/SearchPodcastRecyclerViewAdapter.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import com.wang.avi.AVLoadingIndicatorView
 
 import java.util.ArrayList
 
@@ -26,6 +27,7 @@ import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
+import org.jetbrains.anko.find
 
 /**
  * Created by chuondao on 9/10/17.
@@ -50,88 +52,22 @@ class SearchPodcastRecyclerViewAdapter(podcastList: ArrayList<Podcast>, internal
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val podcast = podcastList[position]
         val podcastVh = holder as PodcastItemViewHolder
+        podcastVh.podcast = podcast
         podcastVh.author.text = podcast.artistName
         podcastVh.title.text = podcast.collectionName
+        podcastVh.itemPos = position
+        podcastVh.progressView.visibility = View.GONE
+        podcastVh.downloadedView.visibility = View.VISIBLE
         // need glide to load the image here
-        val checkPodcastDbObs = viewModel.getPodcastObservable(podcast.collectionId)
-
-        // check to decide where to load image
-        checkPodcastDbObs.observeOn(AndroidSchedulers.mainThread()).subscribe(
-                object : Observer<Podcast> {
-                    override fun onSubscribe(d: Disposable) {
-
-                    }
-                    override fun onNext(it: Podcast) {
-                        // this true mean podcast is already subscribed
-                        var podcastToPass: Podcast? = null
-                        if (it.collectionId.trim { it <= ' ' } != "") {
-                            podcastVh.downloadedView.setImageResource(R.mipmap.ic_downloaded)
-                            // disable on click listener
-                            Log.d(TAG, "Load image from local path ${it.imgLocalPath}")
-                            GlideApp.with(fragment).load(it.imgLocalPath.trim { it <= ' ' }).into(podcastVh.imgView)
-                            podcastToPass = it
-                        } else {
-                            podcastVh.downloadedView.setImageResource(R.mipmap.ic_subscribe)
-                            if (podcast.artworkUrl100 != null) {
-                                GlideApp.with(fragment).load(podcast.artworkUrl100.trim { it <= ' ' }).into(podcastVh.imgView)
-                            }
-                            podcastToPass = podcast
-                        }
-                        // subscribe to listen to change in podcast
-                         subscribeToPodcastUpdate(podcastToPass.collectionId, position)
-                        // set onclickListenter to launch details podcast
-                        val finalPodcastToPass = podcastToPass
-                        podcastVh.itemView.setOnClickListener {
-                            // need to get the podcast from db to make sure it's updated
-                            _ -> viewModel.getPodcastObservable(finalPodcastToPass.collectionId)
-                                    .observeOn(AndroidSchedulers.mainThread())
-                                    .subscribe(
-                                            object : Observer<Podcast> {
-                                                override fun onSubscribe(d: Disposable) {
-
-                                                }
-
-                                                override fun onNext(it: Podcast) {
-                                                    val intent = Intent(this@SearchPodcastRecyclerViewAdapter
-                                                            .ctx, PodcastDetailsActivity::class.java)
-                                                    // just in case the podcast is not in the db
-                                                    if (it.collectionId.trim() == "") {
-                                                        intent.putExtra(PODCAST_KEY, podcast)
-                                                    } else {
-                                                        intent.putExtra(PODCAST_KEY, it)
-                                                    }
-
-                                                    intent.putExtra(ITEM_POS_KEY, position)
-                                                    fragment.activity.startActivityForResult(intent, REQUEST_CODE)
-                                                }
-
-                                                override fun onError(e: Throwable) {
-                                                    e.printStackTrace()
-                                                    Log.e(TAG, "Unexpected Error before launch detailed podcast " + "activity")
-                                                }
-
-                                                override fun onComplete() {
-
-                                                }
-                                            }
-                                    )
-                        }
-                    }
-                    override fun onError(e: Throwable) {
-                        e.printStackTrace()
-                    }
-
-                    override fun onComplete() {
-                    }
-                }
-        )
-
         // set onclick listener for download image locally and insert podcast to db
         podcastVh.downloadedView.setOnClickListener { view ->
             if (!NetworkHelper.isConnectedToNetwork(this@SearchPodcastRecyclerViewAdapter.ctx)) {
                 NetworkHelper.showNetworkErrorDialog(ctx)
                 return@setOnClickListener
             }
+            // show progress view
+            podcastVh.progressView.visibility = View.VISIBLE
+            podcastVh.downloadedView.visibility = View.GONE
             viewModel.getSubscribeObservable(podcast)
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(object : Observer<Boolean> {
@@ -164,29 +100,7 @@ class SearchPodcastRecyclerViewAdapter(podcastList: ArrayList<Podcast>, internal
         return podcastList.size
     }
 
-    private fun subscribeToPodcastUpdate(podcastId: String, pos: Int) {
-        PodcastViewModel.subscribePodcastSubject(
-                object : Observer<PodcastState> {
-                    override fun onSubscribe(d: Disposable) {
-                        disposableList.add(d)
-                    }
-                    override fun onNext(t: PodcastState) {
-                        if (t.uniqueId == podcastId) {
-                            // only the button and state have to change
-                            this@SearchPodcastRecyclerViewAdapter.notifyItemChanged(pos)
-                        }
-                    }
 
-                    override fun onError(e: Throwable) {
-                        e.printStackTrace()
-                    }
-
-                    override fun onComplete() {
-
-                    }
-                }
-        )
-    }
 
     fun cleanUpAllDisposable() {
         for (d in disposableList) {
@@ -201,19 +115,115 @@ class SearchPodcastRecyclerViewAdapter(podcastList: ArrayList<Podcast>, internal
         var author: TextView
         var title: TextView
         var downloadedView: ImageView
+        var progressView: AVLoadingIndicatorView
+        var itemPos: Int = -1
         private var disposable : Disposable? = null
-        private var podcast : Podcast? = null
+        var podcast : Podcast? = null
+            set(value) {
+                // reset disposable
+                if (disposable != null) disposable!!.dispose()
+                disposable = null
+                val podcastVh = PodcastItemViewHolder@this
+                if (value != null) {
+                    viewModel.getPodcastObservable(value.collectionId)
+                            .observeOn(AndroidSchedulers.mainThread()).subscribe(
+                            object : Observer<Podcast> {
+                                override fun onSubscribe(d: Disposable) {
+
+                                }
+
+                                override fun onNext(it: Podcast) {
+                                    // this true mean podcast is already subscribed
+                                    if (it.collectionId.trim { it <= ' ' } != "") {
+                                        podcastVh.downloadedView.setImageResource(R.mipmap.ic_downloaded)
+                                        // disable on click listener
+                                        Log.d(TAG, "Load image from local path ${it.imgLocalPath}")
+                                        GlideApp.with(fragment).load(it.imgLocalPath.trim { it <= ' ' }).into(podcastVh.imgView)
+                                        field = it
+                                    } else {
+                                        podcastVh.downloadedView.setImageResource(R.mipmap.ic_subscribe)
+                                        if (value.artworkUrl100 != null) {
+                                            GlideApp.with(fragment)
+                                                    .load(value.artworkUrl100
+                                                            .trim()).into(podcastVh.imgView)
+                                        }
+                                        field = value
+                                    }
+                                    // set onclickListenter to launch details podcast
+                                    podcastVh.itemView.setOnClickListener {
+                                        // need to get the podcast from db to make sure it's updated
+                                        _ -> viewModel.getPodcastObservable(field!!.collectionId)
+                                            .observeOn(AndroidSchedulers.mainThread())
+                                            .subscribe(
+                                                    object : Observer<Podcast> {
+                                                        override fun onSubscribe(d: Disposable) {
+
+                                                        }
+
+                                                        override fun onNext(it: Podcast) {
+                                                            val intent = Intent(this@SearchPodcastRecyclerViewAdapter
+                                                                    .ctx, PodcastDetailsActivity::class.java)
+                                                            // just in case the podcast is not in the db
+                                                            if (it.collectionId.trim() == "") {
+                                                                intent.putExtra(PODCAST_KEY, podcast)
+                                                            } else {
+                                                                intent.putExtra(PODCAST_KEY, it)
+                                                            }
+                                                            fragment.activity.startActivityForResult(intent, REQUEST_CODE)
+                                                        }
+
+                                                        override fun onError(e: Throwable) {
+                                                            e.printStackTrace()
+                                                            Log.e(TAG, "Unexpected Error before launch detailed podcast " + "activity")
+                                                        }
+
+                                                        override fun onComplete() {
+
+                                                        }
+                                                    }
+                                            )
+                                    }
+                                    // set subscriber for listener
+                                    PodcastViewModel.subscribePodcastSubject(
+                                            object : Observer<PodcastState> {
+                                                override fun onSubscribe(d: Disposable) {
+                                                    disposable = d
+                                                }
+                                                override fun onNext(t: PodcastState) {
+                                                    if (t.uniqueId == field!!.collectionId) {
+                                                        // only the button and state have to change
+                                                        this@SearchPodcastRecyclerViewAdapter.notifyItemChanged(itemPos)
+                                                    }
+                                                }
+
+                                                override fun onError(e: Throwable) {
+                                                    e.printStackTrace()
+                                                }
+
+                                                override fun onComplete() {
+
+                                                }
+                                            }
+                                    )
+                                }
+
+                                override fun onError(e: Throwable) {
+                                    e.printStackTrace()
+                                }
+
+                                override fun onComplete() {
+                                }
+                            }
+                    )
+                }
+            }
 
         init {
             imgView = itemView.findViewById(R.id.podcast_image)
             author = itemView.findViewById(R.id.podcast_author)
             title = itemView.findViewById(R.id.podcast_title)
             downloadedView = itemView.findViewById(R.id.podcast_downloaded_img)
-        }
-        // set podcast to the holder
-        fun setPodcast (podcast: Podcast) {
-            PodcastItemViewHolder@this.podcast = podcast
-
+            progressView = itemView.findViewById(R.id.subscribing_progress_view)
         }
     }
 
@@ -221,7 +231,6 @@ class SearchPodcastRecyclerViewAdapter(podcastList: ArrayList<Podcast>, internal
 
         private val TAG = "PocastAdapter"
         private val PODCAST_KEY = "podcast_key"
-        private val ITEM_POS_KEY = "item_pos_key"
         private val REQUEST_CODE = 1
     }
 }
